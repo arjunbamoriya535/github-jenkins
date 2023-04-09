3db487ee00f1ffbcaf77adc5a7b68b929c35244f
arjunbamoriya535 <55328822+arjunbamoriya535@users.noreply.github.com>
2023-04-09 18:39:40 +0530

    code

---
+++ src/main/java/com/example/sprinjenkinsdemo/SprinJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinjenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinJenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinJenkinsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprinjenkinsdemo/SprinJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinJenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
